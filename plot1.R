c025d6cbea34ec751c554de5e7ee93300af91185
Eiman-Nuaimi <63241611+Eiman-Nuaimi@users.noreply.github.com>
2020-05-18 20:32:16 +0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,7 @@
 plot1<- function(){
               
-        ## read data
         powerdata <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
         
-        ## change class of all columns to correct class
         powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
         powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
         powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
@@ -14,10 +12,9 @@ plot1<- function(){
         powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
         powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
         
-        ## subset data from 2007-02-01 and 2007-02-02
+        
         subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
         
-        ## plot histogram of global active power for those 2 days
         png("plot1.png", width=480, height=480)
         hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
         dev.off()
