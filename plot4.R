ab9a0a98c50fe1ddfa7019900436a0f1368b1b5e
Eiman-Nuaimi <63241611+Eiman-Nuaimi@users.noreply.github.com>
2020-05-18 20:31:17 +0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,13 +1,12 @@
 plot4 <- function() {
           
-        ## read data
+        
         powerdata <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
         
-        ## Create column in table with date and time merged together
         FullTimeDate <- strptime(paste(powerdata$Date, powerdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
         powerdata <- cbind(powerdata, FullTimeDate)
         
-        ## change class of all columns to correct class
+      
         powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
         powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
         powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
@@ -18,10 +17,10 @@ plot4 <- function() {
         powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
         powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
         
-        ## subset data from 2007-02-01 and 2007-02-02
+        
         subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
         
-        ## plot the 4 graphs
+        
         png("plot4.png", width=480, height=480)
         par(mfrow=c(2,2))
         with(subsetdata, plot(FullTimeDate, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
